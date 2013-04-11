c5d600717734623c38307ac0706fe94953cbadd1
Ronny Wang <ronnywang@gmail.com>
2013-04-11 11:53:21 +0800

    popup 頁面加上搜尋公司功能

--- popup.js
+++ popup.js
@@ -37,3 +37,39 @@ $('#package-list').on('change', 'li input:checkbox', function(){
         update_choosed_packages(choosed_packages);
     });
 });
+
+$('.nav-switch').click(function(e){
+    e.preventDefault();
+    var box = $(this).attr('id').split('-')[1];
+    $('.nav-switch').removeClass('active');
+    $('#nav-' + box).addClass('active');
+    $('.div-nav').hide();
+    $('#div-' + box).show();
+});
+
+var search_serial = 0;
+$('#search-word').keyup(function(e){
+    var name = $('#search-word').val();
+    $('#search-list').html('');
+
+    if ('' == name) {
+	return;
+    }
+
+    search_serial ++;
+    (function(current_search_serial){
+	search_name(name, function(package_id, package_row){
+	    get_package_info(function(package_info){
+		if (search_serial != current_search_serial) {
+		    return;
+		}
+		package_info = get_package_info_by_id(package_info, package_id);
+		document.getElementById('search-list').innerHTML += '<li>'
+		+ htmlspecialchars(package_row[0]).replace(name, '<strong>' + name + '</strong>') + ', '
+		+ htmlspecialchars(package_row[1]) + '. ' + htmlspecialchars(package_row[2])
+		+ '[<a href="' + htmlspecialchars(package_info.url) + '" target="_blank">' + htmlspecialchars(package_info.name) + '</a>]'
+		+ '</li>';
+	    });
+	});
+    })(search_serial);
+});
