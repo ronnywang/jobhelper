28c6ef6a801e823f37b11b121022f028f085da54
Ronny Wang <ronnywang@gmail.com>
2013-04-22 11:40:58 +0800

    把資料依照資料包分組，以方便閱讀

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
 var popup_function = function(rows, package_info){
+    // 確認有沒有 #CompanyInfo 的下方視窗
     if (!document.getElementById('CompanyInfo')) {
 	var content = "<div id='CompanyInfo' style='max-height: 20%; overflow-y: scroll; background: #cc103f; bottom: 0; padding: 5px; text-align: left; z-index: 99999; font-size: 14.5px; line-height: 1.5; color: #fff; position: fixed'>"
-	    + "<ol id='CompanyInfoMessage'></ol>"
+	    + "<ul id='CompanyInfoMessage' style='list-style-type: disc'></ul>"
 	    + "<div style='color:#fff;font-weight:bold;float:right;padding-right:8px;width:46px;'>"
 	    + "<span id='CompanyInfoClose' style='cursor:pointer;'>關閉</span>"                
 	    + "</div></div>";
@@ -11,21 +12,24 @@ var popup_function = function(rows, package_info){
 	close.addEventListener('click',function() {
 	    document.getElementById('CompanyInfo').style.display = 'none';
 	});
-    } else {
-	document.getElementById('CompanyInfo').style.display = 'block';
+
+	var info_dom = document.getElementById('CompanyInfo');
+	info_dom.style.background = 'yellow';
+	info_dom.style.color = 'black';
     }
 
-    var htmlspecialchars = function(str){
-	var span_dom = document.createElement('span');
-	span_dom.innerText = str;
-	str = span_dom.innerHTML;
-	delete(span_dom);
-	return str;
-    };
+    // 確認該資料包有沒有 <li>
+    if (!document.getElementById('CompanyInfo-Package-' + package_info.id)) {
+	var content = '';
+	content += '<li>';
+	content += '<a href="' + htmlspecialchars(package_info.url) + '" target="_blank">' + htmlspecialchars(package_info.name) + '</a>';
+	content += '(共 <span id="CompanyInfo-PackageCount-' + package_info.id + '">1</span> 筆符合)';
+	content += '<ol style="list-style-type: decimal" id="CompanyInfo-Package-' + package_info.id +'"></ol>';
+	content += '</li>';
+	document.getElementById('CompanyInfoMessage').innerHTML += content;
+    }
 
-    var info_dom = document.getElementById('CompanyInfo');
-    info_dom.style.background = 'yellow';
-    info_dom.style.color = 'black';
+    // 塞資料
     var content = '';
     content += '<li>';
     content += htmlspecialchars(rows[1]) + '. ' + htmlspecialchars(rows[2]);
@@ -35,10 +39,10 @@ var popup_function = function(rows, package_info){
     if (rows[4]) {
         content += '[<a href="' + htmlspecialchars(rows[4]) + '" target="_blank">截圖</a>]';
     }
-    content += '[資料包: <a href="' + htmlspecialchars(package_info.url + '#company-' + rows[0] + '-' + rows[1]) + '" target="_blank">' + htmlspecialchars(package_info.name) + '</a>]';
     content += '</li>';
 
-    document.getElementById('CompanyInfoMessage').innerHTML += content;
+    document.getElementById('CompanyInfo-Package-' + package_info.id).innerHTML += content;
+    document.getElementById('CompanyInfo-PackageCount-' + package_info.id).innerHTML = document.getElementById('CompanyInfo-Package-' + package_info.id).getElementsByTagName('li').length;
 };
 
 
