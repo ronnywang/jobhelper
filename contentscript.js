b3091495db334255a7cd2c3b51457315e92e0a47
Ronny Wang <ronnywang@gmail.com>
2016-05-14 04:09:50 +0000

    add type number on params.from

--- contentscript.js
+++ contentscript.js
@@ -6,7 +6,7 @@ var get_company_info = function(){
 	// 有 jQuery 可以用
 	var company_dom = jQuery('#comp_header li.comp_name p a', document);
 	if (company_dom.length != 0) {
-	    params.from = '104';
+	    params.from = '104-1';
 	    params.name = company_dom.eq(0).text();
 	    params.company_link = company_dom.eq(0).attr('href');
 	    return params;
@@ -14,7 +14,7 @@ var get_company_info = function(){
 
 	company_dom = jQuery('#comp_header li.comp_name h1', document);
 	if (company_dom.length != 0) {
-	    params.from = '104';
+	    params.from = '104-2';
 	    params.name = company_dom.text();
 	    params.company_link = document.location;
 	    return params;
@@ -23,7 +23,7 @@ var get_company_info = function(){
     // http://www.104.com.tw/job/?jobno=3lluq&jobsource=n104bank1
 	company_dom = jQuery('span.company a:first', document);
 	if (company_dom.length != 0) {
-	    params.from = '104';
+	    params.from = '104-3';
 	    params.name = company_dom.eq(0).text();
 	    params.company_link = company_dom.eq(0).attr('href');
 	    return params;
@@ -35,7 +35,7 @@ var get_company_info = function(){
 	    if (document.location.pathname.match('/cust/view')) {
 		var h1_dom = jQuery('#mainHeader h1.h1');
 		if (h1_dom.length == 1) {
-		    params.from = '104';
+		    params.from = '104-4';
 		    params.name = h1_dom.text();
 		    return params;
 		}
@@ -56,7 +56,7 @@ var get_company_info = function(){
 		    }
 		    params.company_link = a_dom.attr('href');
 		    params.name = a_dom.text();
-		    params.from = '104';
+		    params.from = '104-5';
 		}
 	    }
 
@@ -67,7 +67,7 @@ var get_company_info = function(){
     } else if ('www.taiwanjobs.gov.tw' == document.location.hostname) {
 	var company_dom = jQuery('#divcontent span:first',document);
 	if (company_dom.length != 0) {
-	    params.from = 'ejob';
+	    params.from = 'ejob-1';
 	    params.name = company_dom.text().trim();
 	    return params;
 	}
@@ -86,14 +86,14 @@ var get_company_info = function(){
 	    }
 	    params.company_link = a_dom.attr('href');
 	    params.name = a_dom.text();
-	    params.from = '104temp';
+	    params.from = '104temp-1';
 	}
 
 	return params;
     } else if ('www.yes123.com.tw' == document.location.hostname||'yes123.com.tw' == document.location.hostname) {
             // 處理小而美企業頁面
         if (jQuery('.dtitle').length == 1 && document.location.href.match('small_corp')) {
-            params.from = 'yes123';
+            params.from = 'yes123-1';
             params.name = jQuery('.dtitle').text();
             return params;
         }
@@ -105,11 +105,12 @@ var get_company_info = function(){
 	
 	if (jQuery('.company_title').length==1) {
 		params.name = jQuery('.company_title').text();
+        params.from = 'yes123-2';
 	}else if (jQuery('.jobname_title a:first').length==1) {
 		params.name = jQuery('.jobname_title a:first').text();
+        params.from = 'yes123-3';
 	}
 
-	params.from = 'yes123';
 	params.company_link = matches[1];
 	return params;
     } else if ('www.1111.com.tw' == document.location.hostname) {
@@ -119,7 +120,7 @@ var get_company_info = function(){
         jQuery('#commonTop li a').each(function(){
             var href = $(this).attr('href');
             if ('string' == typeof(href) && href.match('/job-bank/company-description\.asp\\?nNo=[^&]*')) {
-                params.from = '1111';
+                params.from = '1111-1';
 		params.name = $(this).text();
 		params.company_link = $(this).attr('href');
 		found = true;
@@ -131,7 +132,7 @@ var get_company_info = function(){
 	}
 
         if ('object' === typeof(vizLayer) && 'string' === typeof(vizLayer.catname)) {
-            params.from = '1111';
+            params.from = '1111-2';
             params.name = vizLayer.catname;
             params.company_link = '#';
             return params;
@@ -140,7 +141,7 @@ var get_company_info = function(){
 	    var self = $(this);
 
 	    if (self.attr('href').match(/找工作機會/)) {
-		params.from = '1111';
+		params.from = '1111-3';
 		params.name = self.text();
 		params.company_link = self.attr('href');
 		found = true;
@@ -155,7 +156,7 @@ var get_company_info = function(){
 	    var self = $(this);
 
 	    if (self.attr('href').match(/找工作機會/)) {
-		params.from = '1111';
+		params.from = '1111-4';
 		params.name = self.text();
 		params.company_link = self.attr('href');
 		found = true;
@@ -171,7 +172,7 @@ var get_company_info = function(){
         if (decoded_url.match('http://www.1111.com.tw/.*-找工作(機會)?-[0-9]*\.htm')) {
             var h1_doms = jQuery('h1');
             if (h1_doms.length == 1) {
-                params.from = '1111';
+                params.from = '1111-5';
                 params.name = h1_doms.text();
                 return params;
             }
@@ -181,23 +182,23 @@ var get_company_info = function(){
     } else if ('www.518.com.tw' == document.location.hostname) {
         if (jQuery('.company-info h3 a').length) {
             var dom = $('.company-info h3 a');
-            params.from = '518';
+            params.from = '518-1';
             params.name = dom.text();
             return params;
         }
 
         if (jQuery('#company-title').length) {
             if (jQuery('#company-title .comp-name').length == 1) {
-                params.from = '518';
+                params.from = '518-2';
                 params.name = jQuery('#company-title .comp-name').text();
                 return params;
             }
             if (jQuery('#company-title .sTrong').length == 1) {
-                params.from = '518';
+                params.from = '518-3';
                 params.name = jQuery('#company-title .sTrong')[0].childNodes[0].nodeValue.replace(' ', '');
                 return params;
             }
-            params.from = '518';
+            params.from = '518-4';
             params.name = jQuery('#company-title').text().replace('所有工作機會»', '').replace(' ', '');
             params.company_link = document.location.href;
             return params;
@@ -208,7 +209,7 @@ var get_company_info = function(){
 	}
 
 	var dom = jQuery('.company-info h2 a');
-	params.from = '518';
+	params.from = '518-5';
 	params.name = dom.text();
 	params.company_link = dom.attr('href');
     } else {
